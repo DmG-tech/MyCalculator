387210a633ed3a2659075143a551ce90717f5c83
Dima <dm.golubckov@mail.ru>
2020-05-11 11:13:58 +0300

    add negative number

--- app/src/main/java/dmg/project/mycalculator/controller/CommandExecutor.java
+++ app/src/main/java/dmg/project/mycalculator/controller/CommandExecutor.java
@@ -21,49 +21,62 @@ public class CommandExecutor {
         expressionBuilder.clearError();
         switch (operation) {
             case ADDITION:
-            case SUBTRACTION:
             case MULTIPLY:
             case DIVISION:
                 currentOperation = operation;
-                if (expressionBuilder.getString() == null || expressionBuilder.getString().isEmpty()) return;
+                if (expressionBuilder.getString() == null || expressionBuilder.getString().isEmpty())
+                    return;
                 firstArgument = Double.parseDouble(expressionBuilder.getString());
                 expressionBuilder.clearExpression();
                 break;
+            case SUBTRACTION:
+                if (expressionBuilder.getString().isEmpty())
+                    expressionBuilder.updateExpression("-");
+                else {
+                    firstArgument = Double.parseDouble(expressionBuilder.getString());
+                    expressionBuilder.clearExpression();
+                }
+                break;
             case DELETE:
-                if (expressionBuilder.getString() == null || expressionBuilder.getString().isEmpty()) return;
+                if (expressionBuilder.getString() == null || expressionBuilder.getString().isEmpty())
+                    return;
                 expressionBuilder.delChar();
                 break;
             case EQUAL:
-                if (currentOperation == Operation.EQUAL || expressionBuilder.getString() == null || expressionBuilder.getString().isEmpty()) return;
+                if (currentOperation == Operation.EQUAL || expressionBuilder.getString() == null || expressionBuilder.getString().isEmpty())
+                    return;
                 secondArgument = Double.parseDouble(expressionBuilder.getString());
                 expressionBuilder.clearExpression();
                 try {
                     expressionBuilder.updateExpression(expressionBuilder.checkZero(executeArithmeticOperation()));
-                }
-                catch (ArithmeticException e) {
+                } catch (ArithmeticException e) {
                     expressionBuilder.setString("");
                     expressionBuilder.printError(e.getMessage());
                 }
                 currentOperation = operation;
-            break;
+                break;
             case CLEAR:
                 currentOperation = operation;
                 expressionBuilder.clearExpression();
-            break;
+                break;
         }
     }
 
     private Number executeArithmeticOperation() throws ArithmeticException {
         Number result = 0;
         switch (currentOperation) {
-            case ADDITION: result = Calculator.addiction((double) firstArgument, (double) secondArgument);
-            break;
-            case SUBTRACTION: result = Calculator.subtraction((double) firstArgument, (double) secondArgument);
-            break;
-            case DIVISION: result = Calculator.division((double) firstArgument, (double) secondArgument);
-            break;
-            case MULTIPLY: result = Calculator.multiply((double) firstArgument, (double) secondArgument);
-            break;
+            case ADDITION:
+                result = Calculator.addiction((double) firstArgument, (double) secondArgument);
+                break;
+            case SUBTRACTION:
+                result = Calculator.subtraction((double) firstArgument, (double) secondArgument);
+                break;
+            case DIVISION:
+                result = Calculator.division((double) firstArgument, (double) secondArgument);
+                break;
+            case MULTIPLY:
+                result = Calculator.multiply((double) firstArgument, (double) secondArgument);
+                break;
         }
         return result;
     }
